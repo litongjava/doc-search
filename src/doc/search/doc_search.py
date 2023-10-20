362ac33d54d82ca6a8f2717196cb16c04f5bb4c0
litongjava <litongjava@qq.com>
2023-10-20 02:19:43 -1000

    输出详细的异常信息

--- src/doc/search/doc_search.py
+++ src/doc/search/doc_search.py
@@ -1,15 +1,17 @@
 import argparse
 import os
+import traceback
 
+import chardet
 from docx import Document
-from docx.opc.exceptions import PackageNotFoundError
 
 
 def search_keyword_in_word(keywords, file_path):
     try:
         document = Document(file_path)
-    except PackageNotFoundError as err:
-        print(str(err))
+    except Exception as err:
+        stack_trace = traceback.format_exc()
+        print(stack_trace)
         return False
     else:
         keyword_list = [kw.strip() for kw in keywords.split(',')]
@@ -17,6 +19,28 @@ def search_keyword_in_word(keywords, file_path):
             for keyword in keyword_list:
                 if keyword in paragraph.text:
                     return True
+    return False
+
+
+def search_keyword_in_txt(keywords, full_file_path):
+    try:
+        with open(full_file_path, 'rb') as file:
+            byte_content = file.read()
+
+            if not byte_content:  # Check if file is empty
+                return False
+
+            encoding = chardet.detect(byte_content)['encoding']
+            content = byte_content.decode(encoding)
+
+            keyword_list = [kw.strip() for kw in keywords.split(',')]
+            for keyword in keyword_list:
+                if keyword in content:
+                    return True
+    except Exception as e:
+        stack_trace = traceback.format_exc()
+        print(stack_trace)
+        return False
     return False
 
 
@@ -36,4 +60,10 @@ def main():
                 continue
             if filename.endswith('.docx'):
                 if search_keyword_in_word(keywords, full_file_path):
+                    print(full_file_path)
+            if filename.endswith('.md'):
+                if search_keyword_in_txt(keywords, full_file_path):
+                    print(full_file_path)
+            if filename.endswith('.txt'):
+                if search_keyword_in_txt(keywords, full_file_path):
                     print(full_file_path)
